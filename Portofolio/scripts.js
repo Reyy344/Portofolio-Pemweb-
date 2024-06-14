ff6061a3fab2b8c40e35623ea9dd57931e524ec4
Reyy344 <141038093+Reyy344@users.noreply.github.com>
2024-06-14 09:42:28 +0700

    Add files via upload

---
+++ Portofolio/scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form telah dikirim!');
+    // Di sini Anda bisa menambahkan kode untuk mengirim data form ke server atau email.
+});
